0a3a163a60d96725e06cfe27f608cfb61c2c05ee
rozifa <dinbecevic@hflhs-guest-wrls-10-27-60-221.monroe.edu>
2016-02-02 21:43:41 -0500

    sorted boxplots by median

---
+++ boxplots.py
@@ -0,0 +1,18 @@
+def get_teams(dataframe):
+	"""Extracts the team names (column names) from a dataframe"""
+	team_numbers = list(dataframe[:0]) # take the column names and convert them to a list 
+	del team_numbers[0] # remove the text header
+
+def make_data_groups(dataframe, team_names):
+	"""Split a dataframe in to groups based on a list of column names"""
+	master_group = [] # set an empty list to hold multiple lists
+	for name in team_names: # itterate through each teach name
+		group = [] # set an empty list for each team
+		group.append(dataframe[column]) # fill a list with each team's ranks
+		master_group.append(group) # add each of the team lists to the master list
+	return master_group
+
+def make_plots(data_groups):
+	"""Takes data groups created with make_data_groups and creates boxplots from them"""
+	figure = plot.boxplot(data_groups)
+	figure.show()
